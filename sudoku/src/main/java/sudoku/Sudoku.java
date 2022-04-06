b793c2fd636725880425f69edf88df18b356e682
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-04-06 17:50:44 +0200

    Created methods for searching in rows and columns

--- sudoku/src/main/java/sudoku/Sudoku.java
+++ sudoku/src/main/java/sudoku/Sudoku.java
@@ -1,21 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sudoku;
 
-/**
- *
+/*
  * @author Mapacherben
  */
 public class Sudoku {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static final int tamanio = 9;
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        int[][] tablero = {
+            {1, 4, 0, 0, 0, 8, 0, 0, 0},
+            {2, 7, 0, 9, 0, 0, 1, 5, 0},
+            {0, 5, 3, 1, 0, 7, 9, 0, 8},
+            {0, 0, 0, 7, 5, 9, 0, 0, 0},
+            {0, 1, 6, 0, 0, 4, 7, 3, 0},
+            {8, 0, 0, 3, 0, 6, 0, 4, 2},
+            {9, 8, 0, 4, 0, 0, 0, 0, 0},
+            {0, 2, 0, 6, 0, 5, 0, 0, 3},
+            {0, 0, 4, 0, 7, 1, 2, 9, 5}
+        };
     }
-    
+
+    private static boolean numEnFila(int[][] tablero, int fila, int numero) {
+
+        for (int i = 0; i < tamanio; i++) {
+            if (tablero[fila][i] == numero) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean numEnColumna(int[][] tablero, int columna, int numero) {
+
+        for (int i = 0; i < tamanio; i++) {
+            if (tablero[i][columna] == numero) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
