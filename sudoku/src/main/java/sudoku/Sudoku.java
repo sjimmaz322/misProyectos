98867830db11bdd5caf3d1fa33e50d59ba7bd1e5
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-04-06 19:11:35 +0200

    Minor Changes

--- sudoku/src/main/java/sudoku/Sudoku.java
+++ sudoku/src/main/java/sudoku/Sudoku.java
@@ -6,11 +6,12 @@ package sudoku;
 public class Sudoku {
 
     //Como la dimensión de un sudoku es siempre la misma, declaramos al atribute de clase como final.
-    private static final int tamanio = 9;
+    private static final int TAMANIO = 9;
 
     public static void main(String[] args) {
 
         //Creamos el tablero de sudoku, una matriz de 9 por 9.
+
         int[][] tablero = {
             {1, 4, 0, 0, 0, 8, 0, 0, 0},
             {2, 7, 0, 9, 0, 0, 1, 5, 0},
@@ -36,7 +37,7 @@ public class Sudoku {
 
     private static boolean numEnFila(int[][] tablero, int fila, int numero) {//Método para buscar el número en una fila.
 
-        for (int i = 0; i < tamanio; i++) {
+        for (int i = 0; i < TAMANIO; i++) {
             if (tablero[fila][i] == numero) {
                 return true;
             }
@@ -46,7 +47,7 @@ public class Sudoku {
 
     private static boolean numEnColumna(int[][] tablero, int columna, int numero) {//Método para buscar el número en una columna.
 
-        for (int i = 0; i < tamanio; i++) {
+        for (int i = 0; i < TAMANIO; i++) {
             if (tablero[i][columna] == numero) {
                 return true;
             }
@@ -75,10 +76,10 @@ public class Sudoku {
     }
 
     private static boolean resolverSudoku(int[][] tablero) {
-        for (int fila = 0; fila < tamanio; fila++) {
-            for (int columna = 0; columna < tamanio; columna++) {
+        for (int fila = 0; fila < TAMANIO; fila++) {
+            for (int columna = 0; columna < TAMANIO; columna++) {
                 if (tablero[fila][columna] == 0) {
-                    for (int numeroProbar = 1; numeroProbar <= tamanio; numeroProbar++) {
+                    for (int numeroProbar = 1; numeroProbar <= TAMANIO; numeroProbar++) {
                         if (sitioCorrecto(tablero, fila, columna, numeroProbar)) {
                             tablero[fila][columna] = numeroProbar;//Si el número puede ir en la posición lo pone.
 
@@ -97,14 +98,14 @@ public class Sudoku {
         return true;
     }
 
-    private static void imprimirSudoku(int[][] tablero) {
-        for (int fila = 0; fila < tamanio; fila++) {
+    private static void imprimirSudoku(int[][] tablero) {//Método para imprimir el Sudoku
+        for (int fila = 0; fila < TAMANIO; fila++) {
             if (fila % 3 == 0) {
-                System.out.println("---------------");
+                System.out.println("---------------");//Méramente artístico
             }
-            for (int columna = 0; columna < tamanio; columna++) {
+            for (int columna = 0; columna < TAMANIO; columna++) {
                 if (columna % 3 == 0 && columna != 0) {
-                    System.out.print(" | ");
+                    System.out.print(" | ");//Méramente artístico
                 }
                 System.out.print(tablero[fila][columna]);
             }
@@ -112,4 +113,5 @@ public class Sudoku {
         }
         System.out.println("---------------");
     }
+
 }
